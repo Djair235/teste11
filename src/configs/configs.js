8374d309f27a7e4f94eb028ea99b537d5fe25a15
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-11 22:23:46 -0300

    Create configs.js

---
+++ src/configs/configs.js
@@ -0,0 +1,9 @@
+const handlebarsConfig = require("../../src/dependencies/handlebarsConfig")
+const bodyparserConfig = require("../dependencies/bodyparserConfig")
+const corsConfig = require("../dependencies/corsConfig")
+
+module.exports = {
+    handlebarsConfig,
+    bodyparserConfig,
+    corsConfig
+}
