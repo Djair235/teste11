7c87b8ad156931c650ea9d8c004b205ddcb1321f
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-11 23:32:47 -0300

    Update and rename index.js to formulario.js

---
+++ api/formulario.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const handlebars = require('handlebars');
+
+module.exports = async (req, res) => {
+  try {
+    // Leia o conteúdo do arquivo formulario.handlebars
+    const templateSource = fs.readFileSync('views/formulario.handlebars', 'utf-8');
+
+    // Compile o template
+    const template = handlebars.compile(templateSource);
+
+    // Defina os dados a serem inseridos no template
+    const data = {
+      // Seus dados aqui
+    };
+
+    // Renderize o template com os dados
+    const result = template(data);
+
+    // Envie a resposta
+    res.status(200).send(result);
+  } catch (error) {
+    console.error('Erro ao renderizar o template:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+};
