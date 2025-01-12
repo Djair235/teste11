338eb6b20c787b1b62ccac3c90d5ebbf91a9bc2a
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-11 23:02:42 -0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,9 @@
 const mysql2 = require("mysql2/promise")
 
 const connection = mysql2.createPool({
-    host: "localhost",
-    user: "root",
-    password: "13588317",
-    database: "api"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME
 })
 module.exports = connection
