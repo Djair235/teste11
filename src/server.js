e63aa856336a57261a99901056f1ce615be10ff9
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-11 22:20:19 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
+const express = require("express")
+const configs = require("../src/configs/configs")
+const router = require("../src/routers/router")
+const data = require("./others/date")
+const app = express()
 
+configs.corsConfig(app)
+configs.handlebarsConfig(app)
+configs.bodyparserConfig(app)
+app.use(router)
+
+function startServer() {
+    try {
+        app.listen(process.env.PORT || 8081, (error) => {
+            console.log("Servidor rodando em http://localhost:8081")
+            console.log(data.mostrarData())
+        })
+    } catch {
+        console.error("Falha ao iniciar o servidor")
+    }
+}
+startServer()
